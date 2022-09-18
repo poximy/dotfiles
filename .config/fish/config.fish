e129d88e160ce6489d4da3cbd931f026686c0e9d
Erick <76846708+poximy@users.noreply.github.com>
2022-09-18 17:11:57 -0500

    feature: fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+# alias
+alias zshconfig="nvim ~/.zshrc"
+alias fishconfig="nvim ~/.config/fish/config.fish"
+
+alias gitconfig="nvim ~/.gitconfig"
+alias nvimconfig="nvim ~/.config/nvim"
+
+# pnpm
+set -gx PNPM_HOME "/Users/poximy/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
