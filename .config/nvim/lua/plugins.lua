b887913d158539d1c16279b972b25e6d8ed3324c
Erick <76846708+poximy@users.noreply.github.com>
2022-09-18 18:07:56 -0500

    feature: rewrite init.vim to lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,7 @@
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
+	-- lsp & syntax
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
@@ -9,7 +10,7 @@ return require('packer').startup(function(use)
 				ensure_installed = {
 					"go", "python", "typescript", "javascript", "tsx", "html", "css",
 					"svelte", "jsdoc", "json", "prisma", "dockerfile", "vim", "lua",
-					"yaml", "markdown", "markdown_inline", "comment",
+					"yaml", "markdown", "markdown_inline", "comment", "fish",
 				},
 				highlight = {
 					enable = true,
@@ -19,6 +20,95 @@ return require('packer').startup(function(use)
 		end
 	}
 
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use {
+		'hrsh7th/nvim-cmp',
+		config = function()
+			local cmp = require'cmp'
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' }, -- For luasnip users.
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+
+			cmp.setup.filetype('gitcommit', {
+				sources = cmp.config.sources({
+					{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+
+			cmp.setup.cmdline('/', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = 'buffer' }
+				}
+			})
+
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' }
+				}, {
+					{ name = 'cmdline' }
+				})
+			})
+
+			local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local servers = {
+				'tsserver', 'tailwindcss', 'eslint', 'jsonls', 'dockerls',
+				'pyright', 'svelte', 'html', 'cssls'
+			}
+
+			local lspconfig = require('lspconfig')
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup {
+					capabilities = capabilities,
+				}
+			end
+
+			lspconfig.gopls.setup{
+				cmd = { "/Users/poximy/go/bin/gopls" }
+			}
+		end
+	}
+
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+
+	use {
+		'ray-x/go.nvim',
+		config = function()
+			require('go').setup()
+		end
+	}
+	use 'ray-x/guihua.lua'
+
 	-- git
 	use {
 		'lewis6991/gitsigns.nvim',
@@ -27,39 +117,48 @@ return require('packer').startup(function(use)
 		end
 	}
 
-
 	-- UI
 	use {
 		'nvim-lualine/lualine.nvim',
 		config = function()
 			require('lualine').setup{
 				options = {
-					icons_enabled = false, -- nerd font
 					theme = 'auto',
-					component_separators = { left = '|', right = '|'},
+					icons_enabled = true, -- nerd font
+					component_separators = { left = '', right = ''},
 					section_separators = { left = '', right = ''},
 				},
 				sections = {
 					lualine_a = {'mode'},
-					lualine_b = {'branch', 'diff', 'diagnostics'},
-					lualine_c = {'filename'},
+					lualine_b = {'filename'},
+					lualine_c = {'branch', 'diff', 'diagnostics'},
 					lualine_x = {'filesize'},
-					lualine_y = {'encoding'},
+					lualine_y = {'filetype'},
 					lualine_z = {'location'}
 				},
 			}
 		end
 	}
 
-	use {
-		'dracula/vim', as = 'dracula'
-	}
+	use {'dracula/vim', as = 'dracula'}
 
 	-- misc
+	use 'mattn/emmet-vim'
+
 	use {
 		'windwp/nvim-autopairs',
 		config = function()
 			require('nvim-autopairs').setup()
+		end
+	}
+
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- file icons optional
+		},
+		config = function()
+			require("nvim-tree").setup()
 		end
 	}
 end)
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -1,9 +1,8 @@
 -- base config
 vim.opt.title = true
 vim.opt.ignorecase = true
-vim.opt.incsearch = true
 vim.opt.smartcase = true
-vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
 vim.opt.splitbelow = true
 vim.opt.scrolloff = 0
@@ -17,7 +16,7 @@ vim.opt.softtabstop = 2
 vim.opt.swapfile = false
 vim.opt.backup = false
 
--- visual settings
+-- UI settings
 vim.opt.showcmd = true
 vim.opt.showmode = true
 vim.opt.wildmenu = true
@@ -33,5 +32,50 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.lazyredraw = true
 vim.opt.updatetime = 100
 
--- also place hilight group
-vim.cmd[[colorscheme dracula]]
+vim.cmd[[
+hi Normal guibg=NONE ctermbg=NONE
+hi LineNr guibg=NONE ctermbg=NONE
+hi SignColumn guibg=NONE ctermbg=NONE
+hi EndOfBuffer guibg=NONE ctermbg=NONE
+
+colorscheme dracula
+]]
+
+-- keymaps
+local map = require("utils").map
+
+map("n", "<C-t>", ":NvimTreeToggle<CR>")
+
+-- autocomands
+vim.cmd[[
+autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
+autocmd BufWritePre (InsertLeave?) <buffer> lua vim.lsp.buf.formatting_sync(nil,500)
+]]
+
+-- lsp
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
