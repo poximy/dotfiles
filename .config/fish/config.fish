1c82d54cc6299175148e0b2ac2f060328fb50626
Erick <76846708+poximy@users.noreply.github.com>
2022-12-10 18:42:15 -0600

    [refactor] change to nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
 # alias
-alias zshconfig="code ~/.zshrc"
-alias fishconfig="code ~/.config/fish/config.fish"
+alias zshconfig="nvim ~/.zshrc"
+alias fishconfig="nvim ~/.config/fish/config.fish"
 
-alias gitconfig="code ~/.gitconfig"
-alias nvimconfig="code ~/.config/nvim"
+alias gitconfig="nvim ~/.gitconfig"
+alias nvimconfig="nvim ~/.config/nvim"
 
 # pnpm
 set -gx PNPM_HOME "/Users/poximy/Library/pnpm"
